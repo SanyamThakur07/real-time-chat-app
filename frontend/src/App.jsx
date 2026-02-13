7a9c18c719614892599c24fc74c203e56f72db3a
Sanyam Thakur <sanyamthakurr2003@gmail.com>
2026-02-13 19:12:19 +0530

    did some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { ChatPage } from "./pages/ChatPage";
+import { SignUpPage } from "./pages/SignUpPage";
+import { LoginPage } from "./pages/LoginPage";
 
 function App() {
-  return <div className="">App</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/" element={<ChatPage />} />
+    </Routes>
+  );
 }
 
 export default App;
