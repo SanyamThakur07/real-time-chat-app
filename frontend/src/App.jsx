3b23f76a6486dfbc6342fe2a7972ab7199b53ebe
Sanyam Thakur <sanyamthakurr2003@gmail.com>
2026-02-13 18:46:46 +0530

    Revert "added react-router"
    This reverts commit e24fc0a57207ef94c82e2be54295161d8acdc9e5.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,7 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { ChatPage } from "./pages/ChatPage";
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
 
 function App() {
-  return (
-    <div className="bg-red-400">
-      <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </div>
-  );
+  return <div className="">App</div>;
 }
 
 export default App;
