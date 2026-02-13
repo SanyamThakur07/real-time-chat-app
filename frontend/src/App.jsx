e24fc0a57207ef94c82e2be54295161d8acdc9e5
Sanyam Thakur <sanyamthakurr2003@gmail.com>
2026-02-13 18:40:55 +0530

    added react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { ChatPage } from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
-  return <div className="">App</div>;
+  return (
+    <div className="bg-red-400">
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
